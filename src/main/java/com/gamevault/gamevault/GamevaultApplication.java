75d2c3031eb76c0bce44ad426d8d3f20284438b0
dvasq <dvasquezj0510@gmail.com>
2026-08-06 15:06:28 -0500

    Agrego el modelo Game

--- src/main/java/com/gamevault/gamevault/GamevaultApplication.java
+++ src/main/java/com/gamevault/gamevault/GamevaultApplication.java
@@ -9,5 +9,5 @@ public class GamevaultApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GamevaultApplication.class, args);
 	}
-		
+
 }
---
+++ src/main/java/com/gamevault/model/Game.java
@@ -0,0 +1,30 @@
+package com.gamevault.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity //le dice a SpringBoot/Hibernate que esta clase representa una tabla en la BD
+@Table(name = "games") //opcional. Por defecto Hibernate ocupa el nombre de la clase como nombre de la tabla
+
+//Lombok - libreria que elimina codigo repetitivo
+@Data //genera en tiempo de compilacion los getters, setters, equals, hashCode y toString
+@NoArgsConstructor //genera constructor no vacio
+@AllArgsConstructor //genera constructor con todos los parametros
+public class Game {
+
+    @Id //lave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //id generado automaticamente
+    private Long id;
+
+    @Column(nullable = false) //a nivel BD, esta columna no puede ser nula
+    private String title;
+
+    private String developer;
+
+    private Integer releaseYear;
+
+    @Column(length = 1000) //por defecto es VARCHAR(25). Con esto decimos que queremos mas espacio
+    private String description;
+}
