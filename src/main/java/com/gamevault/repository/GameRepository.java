6916b1727e6cb6f0373320aaa7a03ab7741aa3a1
dvasq <dvasquezj0510@gmail.com>
2026-08-07 13:52:46 -0500

    Agrego el service Game

--- src/main/java/com/gamevault/repository/GameRepository.java
+++ src/main/java/com/gamevault/repository/GameRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository //opcional pero buena practica
 //SpringDataJPA genera automaticamente una clase que implementa GameRepository con todos los CRUD
 public interface GameRepository extends JpaRepository<Game, Long> { //JpaRepository - Interfaz generica. Game es el tipo de entidad que maneja, Long es el tipo del campo @Id.
-    
+
 }
---
+++ src/main/java/com/gamevault/service/GameService.java
@@ -0,0 +1,46 @@
+package com.gamevault.service;
+
+import com.gamevault.model.Game;
+import com.gamevault.repository.GameRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service //marca esta clase como un bean gestionado por Spring
+public class GameService {
+
+    //una vez asignado en el constructor, esta referencia no puede volver a cambiar
+    private final GameRepository gameRepository;
+
+    //Este tipo de constructor se llama inyeccion de dependencias por constructor. Nunca se escribe new GameRepository()
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public List<Game> findAll() {
+        return gameRepository.findAll();
+    }
+
+    //Si no existe, lanza una excepcion
+    public Game findById(Long id) {
+        return gameRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Juego no encontrado con id: " + id));
+    }
+
+    public Game create(Game game) {
+        return gameRepository.save(game);
+    }
+
+    public Game update(Long id, Game gameDetails) {
+        Game game = findById(id);
+        game.setTitle(gameDetails.getTitle());
+        game.setDeveloper(gameDetails.getDeveloper());
+        game.setReleaseYear(gameDetails.getReleaseYear());
+        game.setDescription(gameDetails.getDescription());
+        return gameRepository.save(game);
+    }
+
+    public void delete(Long id) {
+        gameRepository.delete(findById(id));
+    }
+}
