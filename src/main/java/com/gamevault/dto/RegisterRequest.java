b76aac7d6d0d7155d72a702c59685afa1bab319a
David Vasquez <dvasquezj0510@gmail.com>
2026-08-29 21:25:39 -0600

    Agrego DTO login

--- src/main/java/com/gamevault/dto/RegisterRequest.java
+++ src/main/java/com/gamevault/dto/RegisterRequest.java
@@ -1,4 +1,23 @@
 package com.gamevault.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+
+    //Usamos NotBlank en vez de NotNull debido a que es mas estricto. Rechaza null, cadena vacía, y cadenas que
+    //solo tengan espacios en blanco.
+    @NotBlank(message = "El username es obligatorio")
+    private String username;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El email debe tener un formato válido")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+    private String password;
 }
